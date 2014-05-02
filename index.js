eb7b8962121f193f566c2eb77472a0daedcbffc5
Ionică Bizău <bizauionica@yahoo.com>
2014-05-02 18:24:45 +0300

    Provide a callback function to off method. Comments.

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-var request = require('request');
-var cheerio = require('cheerio');
+// Dependencies
+var request = require('request')
+  , cheerio = require('cheerio')
+  ;
 
-var off = function(link) {
-    console.log('Requesting...');
+/**
+ *  Offliberty Object
+ *  node-offliberty is a simple interface to the offliberty.com
+ *  download service.
+ *
+ */
+var Offliberty = {
 
-    var options = {
-        url: 'http://offliberty.com/off54.php',
-        method: 'POST',
-        form: { track: link }
-    };
+    /**
+     *  offliberty#off (url, callback)
+     *  Run a request providing the @url. After the response comes
+     *  the @callback function will be called.
+     *
+     *  Arguments
+     *    - url: String representing the the URL you want to get download links for.
+     *    - callback: the callback function
+     *
+     *  Returns the created request.
+     *
+     */
+    off: function(url, callback) {
 
-    function callback(err, res, body) {
-        if (!err && res.statusCode == 200) {
-            var $ = cheerio.load(body);
-            console.log($('.download').attr('href'));
-        } else {
-            console.log(body);
-        }
+        var options = {
+            url: 'http://offliberty.com/off54.php',
+            method: 'POST',
+            form: { track: url }
+        };
+
+        return request(options, function (err, res, body) {
+
+            if (!err && res.statusCode == 200) {
+                var $ = cheerio.load(body);
+                callback (null, $('.download').attr('href'));
+            } else {
+                callback (err || body);
+            }
+        });
     }
+};
 
-    request(options, callback);
-}
-
-exports.off = off;
+module.exports = Offliberty;
